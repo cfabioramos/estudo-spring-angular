47df3fdeeb648d206f679c374ca601a7b35dda37
carlosc <cfabioramos@gmail.com>
2019-03-06 18:52:15 -0300

    Curso Angular #91: Formulários reativos: Sincronizando HTML com FormGroup

--- my-dream-app/src/app/data-form/data-form.module.ts
+++ my-dream-app/src/app/data-form/data-form.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DataFormComponent } from './data-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -9,7 +10,8 @@ import { ReactiveFormsModule } from '@angular/forms';
   ],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    SharedModule
   ],
   exports: [
     DataFormComponent
